575ae63e0ded64ceb1f996dd186a083b6e8f1b28
Ben_Stickelbrucks <ben.stickelbrucks@web.de>
2017-01-31 21:37:44 +0100

    Initial Commit

---
+++ SoundboardConsole/SoundboardConsole/SoundBoard.cs
@@ -0,0 +1,30 @@
+﻿using System.Media;
+using System.Threading;
+using NAudio.Midi;
+
+namespace SoundboardConsole {
+    class SounBoard{
+        static SoundPlayer player;
+        static void Main(){
+            MidiIn midiIN = new MidiIn(0);
+            midiIN.Start();
+            midiIN.MessageReceived += midIn_MessageReceived;
+            new ManualResetEvent(false).WaitOne();
+        }
+
+        private static void midIn_MessageReceived(object sender, MidiInMessageEventArgs e){
+            if(e.MidiEvent != null && e.MidiEvent.CommandCode == MidiCommandCode.AutoSensing)
+                return;
+            if(e.MidiEvent != null && e.MidiEvent.CommandCode == MidiCommandCode.NoteOn){
+                var ne = (NoteOnEvent)e.MidiEvent;
+                try{
+                    string path = $@"D:\Ben\Soundboard\sound{ne.NoteNumber}.wav";
+                    player = new SoundPlayer(path);
+                    player.Play();
+                    player.Dispose();
+                }
+                catch{/*Ignored*/}
+            }
+        }
+    }
+}
