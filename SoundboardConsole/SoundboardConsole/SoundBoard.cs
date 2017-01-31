8e8c2113392b656f2a9b8730cebb5ef7e1d6de6a
Ben_Stickelbrucks <ben.stickelbrucks@web.de>
2017-01-31 22:01:38 +0100

    Commented SoundBoard.cs
    Also fixed an embarrassing typo

--- SoundboardConsole/SoundboardConsole/SoundBoard.cs
+++ SoundboardConsole/SoundboardConsole/SoundBoard.cs
@@ -3,27 +3,27 @@ using System.Threading;
 using NAudio.Midi;
 
 namespace SoundboardConsole {
-    class SounBoard{
+    class SoundBoard {
         static SoundPlayer player;
-        static void Main(){
+        static void Main() {
             MidiIn midiIN = new MidiIn(0);
             midiIN.Start();
-            midiIN.MessageReceived += midIn_MessageReceived;
-            new ManualResetEvent(false).WaitOne();
+            midiIN.MessageReceived += midIn_MessageReceived;    //Start midi and add listener delegate
+            new ManualResetEvent(false).WaitOne();              //We make sure the application does not terminate
         }
 
-        private static void midIn_MessageReceived(object sender, MidiInMessageEventArgs e){
-            if(e.MidiEvent != null && e.MidiEvent.CommandCode == MidiCommandCode.AutoSensing)
-                return;
-            if(e.MidiEvent != null && e.MidiEvent.CommandCode == MidiCommandCode.NoteOn){
+        private static void midIn_MessageReceived(object sender, MidiInMessageEventArgs e) {
+            if (e.MidiEvent != null && e.MidiEvent.CommandCode == MidiCommandCode.AutoSensing)
+                return;                                         //We don't want to deal with midi events not carrying notes
+            if (e.MidiEvent != null && e.MidiEvent.CommandCode == MidiCommandCode.NoteOn) {
                 var ne = (NoteOnEvent)e.MidiEvent;
-                try{
+                try {
                     string path = $@"D:\Ben\Soundboard\sound{ne.NoteNumber}.wav";
                     player = new SoundPlayer(path);
                     player.Play();
-                    player.Dispose();
+                    player.Dispose();                           //Initiate player, fire and forget
                 }
-                catch{/*Ignored*/}
+                catch {/*Ignored*/}
             }
         }
     }
